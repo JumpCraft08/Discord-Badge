d6fcab00fcaec781758a0ff2cd620356b9b10951
JumpCraft08 <angmirfer2008@gmail.com>
2024-10-29 20:44:53 +0100

    License
    License

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import os
 from discord import app_commands, Intents, Client, Interaction
 from dotenv import load_dotenv
 
-# Cargar variables de entorno desde .env
+# Cargar variables de entorno desde el archivo .env
 load_dotenv()
 
+# Obtener el token del bot desde las variables de entorno
 my_secret = os.getenv('TOKEN')
 
+# Definición de la clase principal del bot
 class Bot(Client):
     def __init__(self, *, intents: Intents):
         super().__init__(intents=intents)
@@ -15,19 +17,21 @@ class Bot(Client):
     async def setup_hook(self) -> None:
         await self.tree.sync(guild=None)
 
+# Crear una instancia del bot con intents predeterminados
 bot = Bot(intents=Intents.default())
 
+# Evento que se ejecuta cuando el bot está listo
 @bot.event
 async def on_ready():
     print(f"Conectado como: {bot.user}")
 
+# Definición del comando slash 'givemebadge'
 @bot.tree.command()
 async def givemebadge(interaction: Interaction):
     await interaction.response.send_message("Listo!, espera 24 horas para reclamar la insignia\nPuedes reclamarla aquí: https://discord.com/developers/active-developer")
 
-if my_secret:  # Asegúrate de que el token esté definido
-    bot.run(my_secret)
+# Verificar si el token está definido y ejecutar el bot
+if my_secret:
+    bot.run(my_secret) 
 else:
     print("Error: El token no está definido en las variables de entorno.")
-
-
